3189c763b233a686ea19d0ec3d474fff490f99c2
Soufiane El Ghaddioui <s130669@ap.be>
2023-05-28 22:44:16 +0200

    Powershell script
    Powershell script is nog niet af. Ik moet nog veel opzoekwerk doen over hoe dit exact zou moeten werken. Sorry daarvoor

---
+++ werkende script.ps1
@@ -0,0 +1,13 @@
+﻿Connect-AzureAD
+$voornaam = "Soufiane"  # Vervang dit met jouw eigen voornaam
+
+ 
+
+# Filteren op basis van de voornaam
+$studentenMetZelfdeVoornaam = Get-AzureADUser -Filter "givenName eq '$voornaam'"
+
+ 
+
+# Het aantal studenten met dezelfde voornaam tonen
+$aantalStudenten = $studentenMetZelfdeVoornaam.Count
+Write-Host "Aantal studenten met dezelfde voornaam: $aantalStudenten"
